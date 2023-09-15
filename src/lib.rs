3af5d4a463316648b1006cfd8851b2ee60616397
Pylogmon <pylogmon@outlook.com>
2023-09-15 16:15:00 +0800

    Tatoeba

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,48 @@
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,
+    from: &str,
+    to: &str,
+    needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
-
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => return Err("requestPath not found".into()),
-    };
-
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
-
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
-
+    const URL: &str = "https://tatoeba.org/eng/api_v0/search";
     let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
+        .get(URL)
+        .query(&[
+            ("query", text),
+            ("from", from),
+            ("to", to),
+            ("has_audio", "true"),
+            ("sort", "relevance"),
+        ])
         .send()?
         .json()?;
 
-    fn parse_result(res: Value) -> Option<String> {
-        let result = res.as_object()?.get("translation")?.as_str()?.to_string();
-
-        Some(result.replace("@@", "/"))
+    fn parse_result(res: Value) -> Option<Value> {
+        let results = res.as_object()?.get("results")?.as_array()?;
+        let mut sentence_list = Vec::new();
+        for i in results {
+            let source = i.as_object()?.get("text")?.as_str()?;
+            let mut target = String::new();
+            let translations = i.as_object()?.get("translations")?.as_array()?;
+            for j in translations {
+                for k in j.as_array()? {
+                    target.push_str(k.as_object()?.get("text")?.as_str()?);
+                    target.push_str("\n");
+                }
+            }
+            let sentence = json!({"source":source,"target":target});
+            sentence_list.push(sentence);
+        }
+        Some(json!({"sentence":sentence_list}))
     }
     if let Some(result) = parse_result(res) {
-        return Ok(Value::String(result));
+        return Ok(result);
     } else {
         return Err("Response Parse Error".into());
     }
@@ -47,9 +53,8 @@ mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", needs).unwrap();
-        println!("{result}");
+        let needs = HashMap::new();
+        let result = translate("你好 世界！", "", "eng", needs).unwrap();
+        println!("{result:?}");
     }
 }
